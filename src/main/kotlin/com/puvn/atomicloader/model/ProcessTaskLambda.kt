8c0b114fa3a2defd5cde35ad59d2a1552a134a0b
ELukin <eugeniylukin@gmail.com>
2023-03-12 13:57:53 +0300

    [wip] refactored code related to process per instance service

--- src/main/kotlin/com/puvn/atomicloader/model/ProcessTaskLambda.kt
+++
@@ -1,7 +0,0 @@
-package com.puvn.atomicloader.model
-data class ProcessTaskLambda(val x: Int) {
-    fun invoke() {
-        println("Hello, I'm a lambda with x=$x")
-    }
-
-}
--- src/main/kotlin/com/puvn/atomicloader/service/simulation/impl/ProcessPerInstanceSimulationService.kt
+++ src/main/kotlin/com/puvn/atomicloader/service/simulation/impl/ProcessPerInstanceSimulationService.kt
@@ -4,11 +4,13 @@ import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import com.puvn.atomicloader.config.application_target.ApplicationTargetConfig
 import com.puvn.atomicloader.config.loader.LoaderConfig
 import com.puvn.atomicloader.logging.Logger
-import com.puvn.atomicloader.model.ProcessTaskLambda
+import com.puvn.atomicloader.model.SingleInstanceSimulationProcess
 import com.puvn.atomicloader.service.load.LoadService
 import com.puvn.atomicloader.service.simulation.SimulationService
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty
 import org.springframework.stereotype.Service
+import java.io.File
+import java.io.IOException
 import java.lang.management.ManagementFactory
 
 @Service
@@ -21,54 +23,34 @@ class ProcessPerInstanceSimulationService(
 
     private val mapper = jacksonObjectMapper()
     override fun simulateLoad() {
-        val currentPid = ManagementFactory.getRuntimeMXBean().pid;
+        val currentPid = ManagementFactory.getRuntimeMXBean().pid
         log.info("parent pid is $currentPid, I am preparing child processes")
         //TODO start processes together, not each after each
         for (url in targetConfig.urls) {
-            val process = prepareProcess(ProcessTaskLambda(42) ).start()
-            log.info("process with pid ${process.pid()} has started")
+            exec(SingleInstanceSimulationProcess::class.java, listOf(), listOf("-Xmx200m"))
+            log.info("process for url $url has started")
         }
-
-        /*val javaHome = System.getProperty("java.home")
-        val javaBin = "$javaHome/bin/java"
-        val classpath = System.getProperty("java.class.path")
-        val className = "MyNewJvmClass"
-
-        val builder = ProcessBuilder(
-            javaBin, "-cp", classpath, className)
-
-        try {
-            val process = builder.start()
-        } catch (e: IOException) {
-            e.printStackTrace()
-        }*/
-
-        /*// Serialize the lambda expression and write it to the process's input stream
-        val outputStream = process.outputStream
-        val writer = BufferedWriter(OutputStreamWriter(outputStream))
-        writer.write(serializeLambda(lambda))
-        writer.flush()
-
-// In YourMainClass, read the serialized lambda expression from the input stream
-        val inputStream = System.`in`
-        val reader = BufferedReader(InputStreamReader(inputStream))
-        val serializedLambda = reader.readLine()
-        val lambda = deserializeLambda(serializedLambda)
-
-// Use the lambda expression
-        lambda.invoke()*/
     }
 
-    private fun prepareProcess(task: ProcessTaskLambda): ProcessBuilder {
+    @Throws(IOException::class, InterruptedException::class)
+    fun exec(clazz: Class<*>, args: List<String> = emptyList(), jvmArgs: List<String> = emptyList()): Int {
         val javaHome = System.getProperty("java.home")
-        val javaBin = "$javaHome/bin/java"
+        val javaBin = javaHome + File.separator + "bin" + File.separator + "java"
         val classpath = System.getProperty("java.class.path")
-        val className = "ProcessService"
+        val className = clazz.name
 
-        val taskString = mapper.writeValueAsString(task)
-        return ProcessBuilder(
-            javaBin, "-cp", classpath, className, "--task=$taskString"
-        )
+        val command = ArrayList<String>()
+        command.add(javaBin)
+        command.addAll(jvmArgs)
+        command.add("-cp")
+        command.add(classpath)
+        command.add(className)
+        command.addAll(args)
+
+        val builder = ProcessBuilder(command)
+        val process = builder.inheritIO().start()
+        process.waitFor()
+        return process.exitValue()
     }
 
     companion object {
